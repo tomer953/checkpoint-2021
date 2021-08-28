6fcdb97bff79bcc86ffb902d6ffa70e25c77e184
tomer953 <tomer953@gmail.com>
2021-08-28 12:04:31 +0300

    few changes

--- slot-machine-reloaded/main.py
+++ slot-machine-reloaded/main.py
@@ -26,38 +26,25 @@ def parse_number_to_flag(number,res_str,flag):
 def main():
     predictor = MT19937Predictor()
     flag = "•" * 32
-    print(flag)
-    cookies = {'sessionid': 's6lgjc22o2uyp0vfl47n8fft3b2gfwu6'}
+    cookies = {'sessionid': '60hin9x0m01a5s1docdn0csyj6qy07nf'}
 
     missing_chars = len(flag)
     i=0
     while missing_chars > 0:
-            i+=1
-
-            if i % 50 == 0:
-                print(i)
-            
+            i+=1            
             # get result from server
-            # time.sleep(0.05)
             response = requests.get('http://slot-machine-reloaded.csa-challenge.com/spin/?coins=1', cookies=cookies)
             resJson = response.json()
             res_str = resJson['result']
 
-            # fill 104 numbers to the predictor
-            if i < 105:
-                rnd_num = spin_result_to_number(res_str)
-                predictor.setrandbits(rnd_num,192)
+            # fill 200 known numbers to the predictor (104 is enought, but who cares)
+            if i <= 200:
+                predictor.setrandbits(spin_result_to_number(res_str),192)
             else:
-                # we now should know the next numbers, lets test
+                # we now should know the next numbers, just parse them
                 clone_rnd = predictor.getrandbits(192)
-
-            if i > 200:
-                # print(clone_rnd)
                 flag = parse_number_to_flag(clone_rnd, res_str, flag)
                 print(flag)
                 missing_chars = flag.count("•")
-
-    print(flag)
-
 
 main()
